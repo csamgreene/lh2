02ef85764c37efb77d972c73dd7c1b1639c0a2ad
U-LUXRESEARCHINC\chris.greene <chris.greene@lux-bos-e6320-6.luxresearchinc.com>
2011-09-19 17:42:41 -0400

    Initial commit plus Pages controller and luxhone view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lh2::Application.config.secret_token = 'c273eeda39824cb6305dc5c7a8c7d59c0ee7665bf36fac2691ca0b0301e5e180c4361f8fdd323c601e61c84b84f8e7e2875d48414999588cf070d3547a347881'
